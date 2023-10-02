731cac1901864a23d1f1666f57716d6c5d9934e2
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-10-02 22:47:04 +0300

    сделала 1 шаг дз

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,12 @@
+export const Feedback = () => {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+        </div>
+    
+    );
+  };
+  
---
+++ src/components/Statsistic/Statsitic.js
@@ -0,0 +1,12 @@
+export const Statistic = () => {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <button>Good: 2</button>
+        <button>Neutral:3</button>
+    
+        </div>
+    
+    );
+  };
+  
