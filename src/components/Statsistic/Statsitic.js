d7ad78a038cdd10c3d68f87a82f3cfab6cff7880
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-10-09 10:53:24 +0300

    1

--- src/components/Statsistic/Statsitic.js
+++ src/components/Statsistic/Statsitic.js
@@ -1,7 +1,7 @@
 export const Statistic = () => {
     return (
       <div>
-        <h1>Please leave feedback</h1>
+        <h1>Statistics</h1>
         <button>Good: 2</button>
         <button>Neutral:3</button>
     
